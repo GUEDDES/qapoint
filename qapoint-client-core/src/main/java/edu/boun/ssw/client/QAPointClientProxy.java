66f376f23cf474d6406c9c385436cf470adf1246
serkan.kirbas <serkan.kirbas@gmail.com>
2011-12-05 20:49:02 +0000

    qapoint-client-core

---
+++ qapoint-client-core/src/main/java/edu/boun/ssw/client/QAPointClientProxy.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package edu.boun.ssw.client;
+
+import java.util.List;
+
+/**
+ * @author tosh
+ *
+ */
+public class QAPointClientProxy {
+
+	/**
+	 * 
+	 */
+	public QAPointClientProxy() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void answerReceived(Question question, List<WarmAnswer> answers) {
+		return;
+	}
+
+}
